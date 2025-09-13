2d86b3e112f1351ea4f6aba457e380bd9137141e
Cyndy Pederson <cyndyp@Cyndys-MacBook-Air.local>
2025-09-13 17:24:40 -0500

    Add show/hide password functionality to all password fields

--- script.js
+++ script.js
@@ -368,6 +368,22 @@ function loadInvoicesFromLocalStorage() {
     }
 }
 
+// Password visibility toggle function
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(inputId + '-icon');
+    
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
+
 // API base URL
 const API_BASE = '';
 
